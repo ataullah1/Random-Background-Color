7c08d00198f7f27fc3cdbe28ce6cee3e7a849598
Md Ataullah <ataullahm100@gmail.com>
2023-12-24 18:09:57 +0600

    Color Code Input

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ window.onload = main;
 function main() {
   const bgColorChange = document.querySelector('.main');
   const btn = document.querySelector('#btn');
+  const colorCode = document.querySelector('.colorCode');
 
   btn.addEventListener('click', function () {
     bgColorChange.style.backgroundColor = generateRandomColor();
+    colorCode.value = generateRandomColor().toUpperCase();
   });
 
   bgColorChange.style.backgroundColor = generateRandomColor();
+  colorCode.value = generateRandomColor().toUpperCase();
 }
 
 function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
-  return `rgb(${red},${green},${blue})`;
+  const green = Math.floor(Math.random() * 255);
+  let rgbcolorgenaret = function () {
+    const rgb = `rgb(${red},${green},${blue})`;
+    let rgbinput = document.getElementById('RGBColor');
+    rgbinput.value
+  };
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
 
 /*
@@ -24,7 +32,7 @@ function generateRandomColor() {
 
 
 
-==> Random Color Genret Way-01
+==> Random RGB Color Genret Way-01
 
 const red = Math.floor(Math.random() * 255);
 const blue = Math.floor(Math.random() * 255);
@@ -32,7 +40,7 @@ const green = Math.floor(Math.random() * 255);
 const genaretColor = `rgb(${red},${green},${blue})`;
 
 
-==> Random Color Genret Way-02
+==> Random RGB Color Genret Way-02
 
 function ColorGenret() {
   const red = Math.floor(Math.random() * 255);
@@ -42,12 +50,22 @@ function ColorGenret() {
 }
 
 
-==> Random Color Genret Way-03
+==> Random RGB Color Genret Way-03
 
 function ColorGenret() {
   const randomColor = () => Math.floor(Math.random() * 255);
   return `rgb(${randomColor()},${randomColor()},${randomColor()})`;
 }
+
+==> Random HEX Color Genret Way-04
+
+function ColorGenret() {
+  const red = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
 
 
 
